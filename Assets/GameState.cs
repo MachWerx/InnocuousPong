ed669e0eac59a8613aed8d278d401ecf8292016e
Mach Kobayashi <machk@google.com>
2020-04-18 14:10:13 -0700

    added level based directions

--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -9,13 +9,47 @@ public class GameState : MonoBehaviour {
   [SerializeField] private TMPro.TextMeshPro m_ScoreText = null;
   [SerializeField] private TMPro.TextMeshPro m_LevelText = null;
 
+  public enum Direction {
+    None,
+    Left,
+    Right,
+    Down,
+    Up,
+    Back,
+    Fore
+  }
+
+  // Each enty specifies whether the given paddle (Left, Right, Down, Up, Back, Fore) is static for
+  // that level.
+  private bool[][] kPaddleStaticMap = new bool[][] {
+    new bool[] {false, true },  // level 1
+    new bool[] {false, false},  // level 2
+    new bool[] {false, false},  // level 3
+  };
+
+  // Each enty specifies the horizontal axis for the given paddle (Left, Right, Down, Up, Back,
+  // Fore).
+  private Direction[][] kHorizontalAxisMap = new Direction[][] {
+    new Direction[] {Direction.None, Direction.None, Direction.None, Direction.None, Direction.None, Direction.None},  // level 1
+    new Direction[] {Direction.None, Direction.None, Direction.None, Direction.None, Direction.None, Direction.None},  // level 2
+    new Direction[] {Direction.None, Direction.None, Direction.None, Direction.None, Direction.None, Direction.None},  // level 2
+  };
+
+  // Each enty specifies the vertical axis for the given paddle (Left, Right, Down, Up, Back,
+  // Fore).
+  private Direction[][] kVerticalAxisMap = new Direction[][] {
+    new Direction[] {Direction.Up, Direction.None, Direction.None, Direction.None, Direction.None, Direction.None},  // level 1
+    new Direction[] {Direction.Up, Direction.Up, Direction.None, Direction.None, Direction.None, Direction.None},  // level 2
+    new Direction[] {Direction.Up, Direction.Down, Direction.None, Direction.None, Direction.None, Direction.None},  // level 2
+  };
+
   public int Score {
     get { return m_Score; }
     set {
       m_Score = value;
       m_ScoreText.text = m_Score.ToString();
 
-      Level = (m_Score / 10) + 1;
+      Level = (m_Score / 3) + 1;
     }
   }
 
@@ -26,6 +60,8 @@ public class GameState : MonoBehaviour {
         m_Level = value;
         m_LevelText.text = $"Level {m_Level}!";
         m_LevelText.alpha = 1;
+
+        AdjustPaddles();
       }
     }
   }
@@ -33,15 +69,18 @@ public class GameState : MonoBehaviour {
   private Paddle[] m_Paddles;
   private int m_Level;
   private int m_Score;
+  private float m_Difficulty;
 
-  void Start() {
+  void Awake() {
     m_Paddles = new Paddle[] {
       m_PaddleLeft,
       m_PaddleRight,
       m_PaddleDown,
       m_PaddleUp,
     };
+  }
 
+  private void Start() {
     ResetGame();
   }
 
@@ -62,10 +101,52 @@ public class GameState : MonoBehaviour {
     return m_Paddles;
   }
 
+  public Direction GetHorizontalAxis(Paddle.Type type) {
+    if (Level > 0) {
+      int mapLevel = Level <= kPaddleStaticMap.Length
+          ? Level - 1
+          : kPaddleStaticMap.Length - 1;
+      return kHorizontalAxisMap[mapLevel][(int)type];
+    }
+
+    return Direction.None;
+  }
+
+  public Direction GetVerticalAxis(Paddle.Type type) {
+    if (Level > 0) {
+      int mapLevel = Level <= kPaddleStaticMap.Length
+          ? Level - 1
+          : kPaddleStaticMap.Length - 1;
+      return kVerticalAxisMap[mapLevel][(int)type];
+    }
+
+    return Direction.None;
+  }
+
   private void ResetGame() {
     Score = 0;
     Level = 1;
     m_LevelText.alpha = 1;
+    m_Difficulty = 0.0f;
+
     m_Puck.Reset();
+  }
+
+  private void AdjustPaddles() {
+    float horizontalValue = m_PaddleLeft.transform.localPosition.y;
+    float verticalValue = m_PaddleLeft.transform.localPosition.y;
+    if (Level > 0) {
+      int mapLevel = Level <= kPaddleStaticMap.Length
+          ? Level - 1
+          : kPaddleStaticMap.Length - 1;
+      bool[] staticMap = kPaddleStaticMap[mapLevel];
+      m_PaddleLeft.SetStatic(staticMap[0]);
+      m_PaddleRight.SetStatic(staticMap[1]);
+    }
+
+    foreach (var paddle in GetPaddles()) {
+      paddle.Horizontal = horizontalValue;
+      paddle.Vertical = verticalValue;
+    }
   }
 }
--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -2,7 +2,7 @@
 
 public class Paddle : MonoBehaviour {
   [SerializeField] GameState m_GameState = null;
-  [SerializeField] Type m_Type;
+  [SerializeField] Type m_Type = Type.Left;
   [SerializeField] bool m_IsStatic = false;
 
   public enum Type {
@@ -11,16 +11,109 @@ public class Paddle : MonoBehaviour {
     Down,
     Up,
     Back,
-    Forward
+    Fore
+  }
+
+  public float Horizontal {
+    get {
+      switch (m_GameState.GetHorizontalAxis(m_Type)) {
+        case GameState.Direction.Left:
+          return -transform.localPosition.x;
+        case GameState.Direction.Right:
+          return transform.localPosition.x;
+        case GameState.Direction.Down:
+          return -transform.localPosition.y;
+        case GameState.Direction.Up:
+          return transform.localPosition.y;
+        case GameState.Direction.Back:
+          return -transform.localPosition.z;
+        case GameState.Direction.Fore:
+          return transform.localPosition.z;
+      }
+      return 0;
+    }
+
+    set {
+      Vector3 pos = transform.localPosition;
+      switch (m_GameState.GetHorizontalAxis(m_Type)) {
+        case GameState.Direction.Left:
+          pos.x = -value;
+          break;
+        case GameState.Direction.Right:
+          pos.x = value;
+          break;
+        case GameState.Direction.Down:
+          pos.y = -value;
+          break;
+        case GameState.Direction.Up:
+          pos.y = value;
+          break;
+        case GameState.Direction.Back:
+          pos.z = -value;
+          break;
+        case GameState.Direction.Fore:
+          pos.z = value;
+          break;
+      }
+      transform.localPosition = pos;
+    }
+  }
+
+  public float Vertical {
+    get {
+      switch (m_GameState.GetVerticalAxis(m_Type)) {
+        case GameState.Direction.Left:
+          return -transform.localPosition.x;
+        case GameState.Direction.Right:
+          return transform.localPosition.x;
+        case GameState.Direction.Down:
+          return -transform.localPosition.y;
+        case GameState.Direction.Up:
+          return transform.localPosition.y;
+        case GameState.Direction.Back:
+          return -transform.localPosition.z;
+        case GameState.Direction.Fore:
+          return transform.localPosition.z;
+      }
+      return 0;
+    }
+
+    set {
+      Vector3 pos = transform.localPosition;
+      switch (m_GameState.GetVerticalAxis(m_Type)) {
+        case GameState.Direction.Left:
+          pos.x = -value;
+          break;
+        case GameState.Direction.Right:
+          pos.x = value;
+          break;
+        case GameState.Direction.Down:
+          pos.y = -value;
+          break;
+        case GameState.Direction.Up:
+          pos.y = value;
+          break;
+        case GameState.Direction.Back:
+          pos.z = -value;
+          break;
+        case GameState.Direction.Fore:
+          pos.z = value;
+          break;
+      }
+      transform.localPosition = pos;
+    }
   }
 
   private float kMouseSpeed = 0.5f;
   private float kBorder = 4.75f;
   private Vector3 m_Velocity;
   private float paddleSize;
+  private Vector3 m_InitialPosition;
 
-  void Start() {
+  void Awake() {
     paddleSize = transform.localScale.y;
+    m_InitialPosition = transform.localPosition;
+    Debug.Log($"set init: {m_InitialPosition} {name}");
   }
 
   void Update() {
@@ -28,17 +121,18 @@ public class Paddle : MonoBehaviour {
     Vector3 pos = transform.localPosition;
 
     if (!m_IsStatic) {
-      pos.y += kMouseSpeed * (Input.GetAxis("Mouse Y"));
+      Vector3 oldPos = transform.localPosition;
+      Vertical += kMouseSpeed * (Input.GetAxis("Mouse Y"));
+      Debug.Log($"vert: {Vertical}");
       float boundary = kBorder - paddleSize / 2;
 
-      if (pos.y < -boundary) {
-        pos.y = -boundary;
-      } else if (pos.y > boundary) {
-        pos.y = boundary;
+      if (Vertical < -boundary) {
+        Vertical = -boundary;
+      } else if (Vertical > boundary) {
+        Vertical = boundary;
       }
 
-      m_Velocity = (pos - transform.localPosition) / Time.deltaTime;
-      transform.localPosition = pos;
+      m_Velocity = (transform.localPosition - oldPos) / Time.deltaTime;
     }
   }
 
@@ -100,5 +194,25 @@ public class Paddle : MonoBehaviour {
       // add to the score
       m_GameState.Score++;
     }
+  }
+
+  public void SetStatic(bool value) {
+    m_IsStatic = value;
+    if (m_IsStatic) {
+      transform.localPosition = m_InitialPosition;
+      Debug.Log($"read init: {m_InitialPosition} {name}");
+    }
+    Vector3 size = 0.5f * Vector3.one;
+    switch (m_Type) {
+      case Type.Left:
+      case Type.Right:
+        size.y = m_IsStatic ? 10.5f : 2.0f;
+        break;
+      case Type.Down:
+      case Type.Up:
+        size.x = m_IsStatic ? 10.5f : 2.0f;
+        break;
+    }
+    transform.localScale = size;
   }
 }
