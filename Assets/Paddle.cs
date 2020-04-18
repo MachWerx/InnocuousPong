8f4c579bd9303a4468abb22c1af7cd6411e86b48
Mach Kobayashi <machk@google.com>
2020-04-18 10:25:50 -0700

    added physics

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -7,13 +7,11 @@ public class Paddle : MonoBehaviour {
   private Vector3 m_Velocity;
   private float paddleSize;
 
-  // Start is called before the first frame update
   void Start() {
     m_MousePositionPrev = Input.mousePosition;
     paddleSize = transform.localScale.y;
   }
 
-  // Update is called once per frame
   void Update() {
     Vector2 mousePosition = Input.mousePosition;
     Vector3 pos = transform.localPosition;
@@ -33,16 +31,60 @@ public class Paddle : MonoBehaviour {
 
   }
 
-  public Vector3 CheckIntersection(Vector3 puckPos, Vector3 puckSize, Vector3 puckVel) {
+  public void DoIntersection(ref Vector3 puckPos, Vector3 puckSize, ref Vector3 puckVel) {
     Bounds puckBounds = new Bounds(puckPos, puckSize);
     Vector3 pos = transform.localPosition;
     Vector3 size = transform.localScale;
     Bounds bounds = new Bounds(pos, size);
 
     if (bounds.Intersects(puckBounds)) {
-      return (puckPos - pos).normalized;
-    }
+      // find intersection time
+      float minT = -Time.deltaTime;
+      float maxT = 0;
+      float t = 0.5f * (minT + maxT);
+      for (int i = 0; i < 10; i++) {
+        puckBounds = new Bounds(puckPos + t * puckVel, puckSize);
+        bounds = new Bounds(pos + t * m_Velocity, size);
+        if (bounds.Intersects(puckBounds)) {
+          maxT = t;
+        } else {
+          minT = t;
+        }
+        t = 0.5f * (minT + maxT);
+      }
 
-    return Vector3.zero;
+      // rewind to intersection time
+      pos += t * m_Velocity;
+      bounds = new Bounds(pos, size);
+      puckPos += t * puckVel;
+
+      // figure out normal at intersection point
+      Vector3 normal = Vector3.zero;
+      if (puckPos.x < bounds.min.x) {
+        normal += Vector3.left;
+      } else if (puckPos.x > bounds.max.x) {
+        normal += Vector3.right;
+      }
+      if (puckPos.y < bounds.min.y) {
+        normal += Vector3.down;
+      } else if (puckPos.y > bounds.max.y) {
+        normal += Vector3.up;
+      }
+      if (puckPos.z < bounds.min.z) {
+        normal += Vector3.back;
+      } else if (puckPos.z > bounds.max.z) {
+        normal += Vector3.forward;
+      }
+      normal.Normalize();
+
+      // calculate new puck velocity
+      puckVel -= m_Velocity;
+      puckVel -= 2 * Vector3.Dot(puckVel, normal) * normal;
+      puckVel += m_Velocity;
+
+      // fast foward to current time
+      pos -= t * m_Velocity;
+      puckPos -= t * puckVel;
+    }
   }
 }
--- Assets/Puck.cs
+++ Assets/Puck.cs
@@ -6,26 +6,18 @@ public class Puck : MonoBehaviour {
   private float kBorder = 4.5f;
   private Vector3 m_Velocity;
 
-  // Start is called before the first frame update
   void Start() {
     float angle = (Random.value - 0.5f) * 0.5f * Mathf.PI + Mathf.PI;
     m_Velocity = 3f * new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
   }
 
-  // Update is called once per frame
-  void Update() {
+  // Do physics in late update so that the puck comes after the paddle.
+  void LateUpdate() {
     Vector3 pos = transform.localPosition;
     pos += m_Velocity * Time.deltaTime;
 
     if (pos.x < -kBorder) {
-      Vector3 bounce = m_PaddleLeft.CheckIntersection(pos, transform.localScale, m_Velocity);
-      if (bounce != Vector3.zero) {
-        // t is the amount of time you have to go back to get to the intersection point.
-        float t = (-kBorder - pos.x) / m_Velocity.x;
-        pos += m_Velocity * t;
-        m_Velocity = m_Velocity.magnitude * bounce;
-        pos += m_Velocity * (Time.deltaTime - t);
-      }
+      m_PaddleLeft.DoIntersection(ref pos, transform.localScale, ref m_Velocity);
     } else if (pos.x > kBorder) {
       pos.x = 2 * kBorder - pos.x;
       m_Velocity.x *= -1;
