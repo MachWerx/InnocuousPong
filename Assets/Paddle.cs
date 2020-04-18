0c513055727224e1427fc5eae690b6764712ffdf
Mach Kobayashi <machk@google.com>
2020-04-17 22:03:16 -0700

    added paddle physics

---
+++ Assets/Paddle.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+  private float kMouseSpeed = 0.02f;
+  private float kBorder = 4.75f;
+  private Vector2 m_MousePositionPrev;
+  private float paddleSize;
+
+  // Start is called before the first frame update
+  void Start() {
+    m_MousePositionPrev = Input.mousePosition;
+    paddleSize = transform.localScale.y;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    Vector2 mousePosition = Input.mousePosition;
+    Vector3 pos = transform.localPosition;
+    Debug.Log($"mouse y: {pos.y}");
+    pos.y += kMouseSpeed * (mousePosition.y - m_MousePositionPrev.y);
+
+    float boundary = kBorder - paddleSize / 2;
+
+    if (pos.y < -boundary) {
+      pos.y = -boundary;
+    } else if (pos.y > boundary) {
+      pos.y = boundary;
+    }
+
+    transform.localPosition = pos;
+    m_MousePositionPrev = mousePosition;
+
+  }
+
+  public Vector3 CheckIntersection(Vector3 puckPos, float puckSize) {
+    Vector3 pos = transform.localPosition;
+    Vector3 size = transform.localScale;
+    if (puckPos.x - puckSize < pos.x + size.x &&
+        puckPos.x + puckSize > pos.x - size.x &&
+        puckPos.y - puckSize < pos.y + size.y &&
+        puckPos.y + puckSize > pos.y - size.y &&
+        puckPos.z - puckSize < pos.z + size.z &&
+        puckPos.z + puckSize > pos.z - size.z) {
+      return Vector3.one;
+    }
+
+    return Vector3.zero;
+  }
+}
--- Assets/Puck.cs
+++ Assets/Puck.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 
 public class Puck : MonoBehaviour {
-  private Vector3 m_Velocity;
+  [SerializeField] Paddle m_PaddleLeft;
+
   private float kBorder = 4.5f;
+  private float m_Size;
+  private Vector3 m_Velocity;
 
   // Start is called before the first frame update
   void Start() {
-
+    m_Size = transform.localScale.x;
     float angle = (Random.value - 0.5f) * 0.5f * Mathf.PI + Mathf.PI;
     m_Velocity = 2 * new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
   }
@@ -17,8 +20,10 @@ public class Puck : MonoBehaviour {
     pos += m_Velocity * Time.deltaTime;
 
     if (pos.x < -kBorder) {
-      pos.x = -2 * kBorder - pos.x;
-      m_Velocity.x *= -1;
+      if (m_PaddleLeft.CheckIntersection(pos, m_Size) == Vector3.one) {
+        pos.x = -2 * kBorder - pos.x;
+        m_Velocity.x *= -1;
+      }
     } else if (pos.x > kBorder) {
       pos.x = 2 * kBorder - pos.x;
       m_Velocity.x *= -1;
