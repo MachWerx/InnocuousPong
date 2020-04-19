43bd77a19d5e5e5cc0a2be373ec72c67f89d8ae4
Mach Kobayashi <machk@google.com>
2020-04-19 10:04:22 -0700

    added camera controls

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+  [SerializeField] private Camera m_Camera = null;
+
+  private Quaternion m_SrcRot;
+  private Quaternion m_DstRot;
+  private float m_TransitionValue;
+
+  void Start() {
+    m_TransitionValue = 1;
+  }
+
+  void Update() {
+    if (m_TransitionValue < 1) {
+      m_TransitionValue += 3 * Time.deltaTime;
+      if (m_TransitionValue > 1) {
+        m_TransitionValue = 1;
+      }
+      transform.localRotation = Quaternion.Slerp(m_SrcRot, m_DstRot, m_TransitionValue);
+    }
+  }
+
+  public void TransitionTo(Quaternion dstRot) {
+    m_SrcRot = transform.localRotation;
+    m_DstRot = dstRot;
+    m_TransitionValue = 0;
+  }
+}
